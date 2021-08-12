5a6840b91b35746ec9f08971ce6e3a68ccac9260
nhuse <nathan.huse@yahoo.com>
2021-08-12 12:06:15 -0500

    commit tree 5ec6a3269e60d4d26131ef9bbdfbb0a93639d5c2

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def first_name
+        @first_name
+    end
+    def last_name
+        @last_name
+    end
 
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
